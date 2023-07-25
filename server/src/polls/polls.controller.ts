203dd5723ae0ff9857cdefc568d1c5b0d2dc3bed
HdThinh6102git <lck0601111202@gmail.com>
2023-07-25 15:15:44 +0700

    creating authorize rejoin poll route guard

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,7 +1,9 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, Req, UseGuards} from '@nestjs/common';
 import {CreatePollDto} from "./dtos/CreatePoll.dto";
 import {JoinPollDto} from "./dtos/JoinPoll.dto";
 import {PollsService} from "./polls.service";
+import {ControllerAuthGuard} from "./controller-auth.guard";
+import {RequestWithAuth} from "./types";
 
 @Controller('polls')
 export class PollsController {
@@ -19,13 +21,14 @@ export class PollsController {
 
         return result;
     }
-
+    @UseGuards(ControllerAuthGuard)
     @Post('/rejoin')
-    async rejoin() {
+    async rejoin(@Req() request: RequestWithAuth) {
+        const { userID, pollID, name } = request; // destruct to get value from request
         const result = await this.pollsService.rejoinPoll({
-            name: 'From token',
-            pollID: 'Also from token',
-            userID: 'Guess where this comes from?',
+            name,
+            pollID,
+            userID,
         });
 
         return result;
